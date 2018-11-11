b23ca39c85739ee0f2fcd1812accbded2684d110
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2018-11-11 05:05:32 -0500

    chore: first comit

---
+++ README.md
@@ -0,0 +1,7 @@
+# uSTL
+a very tiny port of STL for PlatformIO-supported boards.
+
+
+```cpp
+#include <uSTL.h>
+```
---
+++ src/uSTL.h
@@ -0,0 +1,10 @@
+
+#include <array>
+#include <cassert>
+#include <cctype>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <memory>
+#include "details/to_string.h"
